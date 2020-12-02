6bbf561e6041a69d90ecf93a9ba119a630e0bb3d
Alexa Summers <alexas0202@gmail.com>
2020-12-01 22:56:27 -0600

    Created Example1 Script

---
+++ Serialization/Assets/Scripts/Example1.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example1 : MonoBehaviour
+{
+  [SerializeField] private int serializedPrivateInt = 3;
+  [SerializeField] private float serializedPrivateFloat = 5.6f;
+  public float publicFloat = 16.52f;
+  private int privateInt = 11;
+  public static int publicStaticInt = 234;
+  public const int publicConstInt = 10;
+  public readonly int publicReadonlyInt = 56;
+}
