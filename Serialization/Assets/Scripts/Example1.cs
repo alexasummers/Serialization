f3bfcbbde80f49e8f2c4a2cb35b4da582e167665
Alexa Summers <alexas0202@gmail.com>
2020-12-01 23:09:15 -0600

    Added example1a

--- Serialization/Assets/Scripts/Example1.cs
+++ Serialization/Assets/Scripts/Example1.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class Example1 : MonoBehaviour
 {
-  [SerializeField] private int serializedPrivateInt = 3;
-  [SerializeField] private float serializedPrivateFloat = 5.6f;
-  public float publicFloat = 16.52f;
-  private int privateInt = 11;
-  public static int publicStaticInt = 234;
-  public const int publicConstInt = 10;
-  public readonly int publicReadonlyInt = 56;
+    [SerializeField]
+    private int serializedPrivateInt = 3;
+
+    [SerializeField]
+    private float serializedPrivateFloat = 6.6f;
+
+    public float publicFloat = 16.52f;
+
+    private int privateInt = 11;
+
+    public static int publicStaticInt = 234;
+
+    public const int publicConstInt = 10;
+
+    public readonly int publicReadonlyInt = 56;
 }
