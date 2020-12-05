b6f5aba2be99099a638e3c8cb0f6a44ffdfd38b5
Alexa Summers <alexas0202@gmail.com>
2020-12-04 23:46:08 -0600

    Add Sprite Prefabs
    Add Sprite Prefabs

---
+++ Serialization/Assets/Scripts/platypus_spawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platypus_spawner : MonoBehaviour
+{
+    private int rand;
+    public Sprite[] Sprite_Pic;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Change();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0));{
+            Change();
+        }
+    }
+
+    void Change(){
+        rand = Random.Range(0,Sprite_Pic.Length);
+        GetComponent<SpriteRenderer>().sprite = Sprite_Pic[rand];
+    }
+}
