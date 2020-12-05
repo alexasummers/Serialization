0771353a0e216031d9c0e6e7fa461a8e30e241a8
Alexa Summers <alexas0202@gmail.com>
2020-12-05 00:17:30 -0600

    Create GameSave script
    Create GameSave script to add serialization

---
+++ Serialization/Assets/Scripts/GameSave.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSave : MonoBehaviour
+{   public GameObject player;
+
+    // Start is called before the first frame update
+    void Start() //restore state of the object
+    {
+        RestoreGame();
+    }
+    void RestoreGame() {
+
+    }
+ 
+    void Update() //checking for a refresh of the save trigger
+    {
+        if (Input.GetKeyDown(KeyCode.S)) {
+            SaveGame();
+        }
+    }
+
+    void SaveGame(){
+
+    }
+}
