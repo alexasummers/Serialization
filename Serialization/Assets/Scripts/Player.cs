780ae056780f3d897367cb9c9d1188f380bd1cf4
Alexa Summers <alexas0202@gmail.com>
2020-12-09 01:03:30 -0600

    Delete Player and Add Controller
    Delete Player and add controller

--- Serialization/Assets/Scripts/Player.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Player{
-
-    public Color color = new Color(23, 4, 1, 1);
-    
-}
---
+++ Serialization/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed =0.1f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Vector 2 position - transform.position;
+            position.x += speed;
+            transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Vector 2 position - transform.position;
+            position.x -= speed;
+            transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            Vector 2 position - transform.position;
+            position.y += speed;
+            transform.position = position;
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            Vector 2 position - transform.position;
+            position.y -= speed;
+            transform.position = position;
+        }
+    }
+}
