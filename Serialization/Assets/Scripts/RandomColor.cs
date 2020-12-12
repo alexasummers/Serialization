d6e77d7cf192c486a175dc5bf877ff6758b9a98d
Alexa Summers <alexas0202@gmail.com>
2020-12-12 01:27:39 -0600

    Update RandomColor and SavePosition
    Clean up randomcolor and saveposition

--- Serialization/Assets/Scripts/RandomColor.cs
+++ Serialization/Assets/Scripts/RandomColor.cs
@@ -8,7 +8,6 @@ public class RandomColor : MonoBehaviour
 {
     public Color[] ColorBank;
 
-    // Start is called before the first frame update
     void Start()
     {
 
--- Serialization/Assets/Scripts/SavePosition.cs
+++ Serialization/Assets/Scripts/SavePosition.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
 public class SavePosition
 {
     public float x;
